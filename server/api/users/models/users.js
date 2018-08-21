b0f9aa73fef9392672a23a3e943fdc1a0709e5b5
Guru Raj <guru@aumentar.io>
2018-08-21 15:50:59 +0530

    model has been changed.

--- server/api/users/models/users.js
+++ server/api/users/models/users.js
@@ -3,25 +3,22 @@ const Schema = mongoose.Schema;
 
 // user schema
 const UserSchema = new Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	},
-	avatar: {
-		type: String
-	},
-	date: {
-		type: Date,
-		default: Date.now
-	}
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('users', UserSchema);
